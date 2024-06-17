374a3b1e3e51f7a0242f7a22b33be7a758283177
Oki Firdaus Syah Putra <85375019+okifirsyah404@users.noreply.github.com>
2024-06-18 00:58:30 +0700

    Update image-multipart.ts

--- src/utils/helpers/multipart/image-multipart.ts
+++ src/utils/helpers/multipart/image-multipart.ts
@@ -1,5 +1,4 @@
 import { UploadFilterFile } from '@blazity/nest-file-fastify';
-import { UnsupportedMediaTypeException } from '@nestjs/common';
 import { FastifyRequest } from 'fastify';
 import { join } from 'path';
 
@@ -13,21 +12,21 @@ export class ImageMultipart {
     req: FastifyRequest,
     file: UploadFilterFile,
   ): boolean {
-    if (!file.mimetype.includes('image')) {
-      throw new UnsupportedMediaTypeException(
-        'Image file is not supported. Please upload an image file.',
-      );
-    }
+    // if (!file.mimetype.includes('image')) {
+    //   throw new UnsupportedMediaTypeException(
+    //     'Image file is not supported. Please upload an image file.',
+    //   );
+    // }
 
-    if (
-      !file.mimetype.includes('jpg') &&
-      !file.mimetype.includes('jpeg') &&
-      !file.mimetype.includes('png')
-    ) {
-      throw new UnsupportedMediaTypeException(
-        'Image file is not supported. Please upload a jpg, jpeg, or png file.',
-      );
-    }
+    // if (
+    ///// !file.mimetype.includes('jpg') &&
+    ///// !file.mimetype.includes('jpeg') &&
+    ///// !file.mimetype.includes('png')
+    // ) {
+    //   throw new UnsupportedMediaTypeException(
+    //     'Image file is not supported. Please upload a jpg, jpeg, or png file.',
+    //   );
+    // }
 
     return true;
   }

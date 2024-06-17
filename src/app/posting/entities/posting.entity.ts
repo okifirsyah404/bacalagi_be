95d5c35e6059f9e954a539db9bb9add764944ad1
AnnasSholahudin <06annassholahudin9b@gmail.com>
2024-06-17 19:39:18 +0700

    push entities

--- src/app/posting/entities/posting.entity.ts
+++ src/app/posting/entities/posting.entity.ts
@@ -1,1 +1,28 @@
-export class Posting {}
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class Posting {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  author: string;
+
+  @Column()
+  publisher: string;
+
+  @Column()
+  yearPublished: number;
+
+  @Column()
+  price: number;
+
+  @Column({ nullable: true })
+  imageUrl?: string; // Optional property for image URL
+}

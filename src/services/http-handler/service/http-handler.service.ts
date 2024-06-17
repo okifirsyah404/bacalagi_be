db77d6c2c2abf28118e2a741f22f0a3fcc653720
Oki Firdaus Syah Putra <85375019+okifirsyah404@users.noreply.github.com>
2024-06-17 20:59:29 +0700

    Update http-handler.service.ts

--- src/services/http-handler/service/http-handler.service.ts
+++ src/services/http-handler/service/http-handler.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class HttpHandlerService {}
+export class HttpHandlerService {
+  // const toModel1 = await this.httpService.axiosRef(
+  //     'http://localhost:3000/detect-image',
+  //     {
+  //       method: 'POST',
+  //       headers: { 'Content-Type': 'multipart/form-data' },
+  //       data: {
+  //         file: fs.createReadStream(
+  //           `${process.cwd()}/public/image/post/${postid}.png}]`,
+  //         ),
+  //       },
+  //     },
+  //   );
+  //   const toModel2 = await this.httpService.axiosRef(
+  //     'http://localhost:3000/predict-price',
+  //     {
+  //       method: 'POST',
+  //       headers: { 'Content-Type': 'application/json' },
+  //       data: {
+  //         ratio_ripped: 0.5,
+  //         ratio_worn_out: 0.5,
+  //         price: 100000,
+  //       },
+  //     },
+  //   );
+}

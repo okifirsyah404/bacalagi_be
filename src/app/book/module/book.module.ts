f48be6a2ae0eb33eadfcb475d92255e22a36c476
Jihan Putri Ratu S <jihanptrrtu@gmail.com>
2024-06-18 18:31:22 +0700

    Update book.module.ts

--- src/app/book/module/book.module.ts
+++ src/app/book/module/book.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { BookController } from '../controller/book.controller';
 import { BookService } from '../service/book.service';
 import { HttpModule } from '@nestjs/axios';
+import { UploadHandlerService } from 'src/services/upload-handler/service/upload-handler.service';
 
 @Module({
   imports: [HttpModule],
   controllers: [BookController],
-  providers: [BookService],
+  providers: [BookService, UploadHandlerService],
 })
 export class BookModule {}
